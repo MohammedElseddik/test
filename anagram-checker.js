e0968d91f7d55f8fa722c0d6522523b592d67a8a
MohammedElseddik <rebbouh.mohammed.elseddik@gmail.com>
2022-02-25 19:38:00 +0100

    Fixing test errors

---
+++ anagram-checker.js
@@ -0,0 +1,12 @@
+module.exports = function scanString(str) {
+  // Write your code here
+  return str.toLowerCase().replace(/[^a-zd]/g, '').split('').sort().join('');
+}
+
+function isAnagram(stringA, stringB) {
+  if (scanString(stringA) == scanString(stringB)) {
+    return true;
+  } else {
+    return false;
+  }
+};
