b91d60e5bba34577e1ad2b9345a9117038be27b6
MohammedElseddik <rebbouh.mohammed.elseddik@gmail.com>
2022-02-25 19:58:35 +0100

    Fixing test errors

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,12 +1,28 @@
-module.exports = function scanString(str) {
+module.exports = function createCharMap(str) {
   // Write your code here
-  return str.toLowerCase().replace(/[^a-zd]/g, '').split('').sort().join('');
-}
+  const map = {};
+  const normalizedString = str.replace(/[^\w]/g, "").toLowerCase();
+
+  for (let char of normalizedString) {
+    map[char] = map[char] + 1 || 1;
+  }
+
+  return map;
+};
 
 function isAnagram(stringA, stringB) {
-  if (scanString(stringA) === scanString(stringB)) {
-    return true;
-  } else {
+  const charMapA = createCharMap(stringA);
+  const charMapB = createCharMap(stringB);
+
+  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
     return false;
   }
-};
+
+  for (let char in charMapA) {
+    if (charMapA[char] !== charMapB[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
